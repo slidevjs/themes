353bebbf86811b9dc47e47e2e00815aaee22a765
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-11 05:04:18 +0800

    fix: release script

--- scripts/prompt.mjs
+++ scripts/prompt.mjs
@@ -1,11 +1,10 @@
 // @ts-nocheck
 
-import { $, cd } from 'zx'
+import { execSync } from 'child_process'
 import { promptForPackage } from './packages.mjs'
 
 const pkg = await promptForPackage()
 
-cd(pkg.path)
 const msg = `chore: release ${pkg.name} v`
 const tag = `${pkg.name}@v`
-await $`bumpp --push --commit ${msg} --tag ${tag}`
+execSync(`pnpx bumpp --push --commit "${msg}" --tag "${tag}"`, { stdio: 'inherit', cwd: pkg.path })
