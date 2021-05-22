4865182f1d4df14542d878b2178274211cd59d09
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-22 17:44:01 +0800

    chore: update changelog locally

--- scripts/prompt.mjs
+++ scripts/prompt.mjs
@@ -1,10 +1,17 @@
 // @ts-nocheck
 
 import { execSync } from 'child_process'
+import { cd, $ } from 'zx'
 import { promptForPackage } from './packages.mjs'
 
 const pkg = await promptForPackage()
 
 const msg = `chore: release ${pkg.name} v`
 const tag = `${pkg.name}@v`
-execSync(`pnpx bumpp --push --commit "${msg}" --tag "${tag}"`, { stdio: 'inherit', cwd: pkg.path })
+execSync('pnpx bumpp', { stdio: 'inherit', cwd: pkg.path })
+cd(pkg.path)
+await $`pnpx conventional-changelog -p angular -i CHANGELOG.md -s --commit-path . --lerna-package ${pkg.name}`
+await $`git add .`
+await $`git commit -m ${msg}`
+await $`git tag ${tag}`
+await $`git push --follow-tags`
--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,11 +1,5 @@
 // @ts-nocheck
 
-import { $, cd } from 'zx'
-import { getPackageInfo } from './packages.mjs'
-
-for (const pkg of await getPackageInfo()) {
-  cd(pkg.path)
-  await $`pnpx conventional-changelog -p angular -i CHANGELOG.md -s --commit-path . --lerna-package ${pkg.name}`
-}
+import { $ } from 'zx'
 
 await $`pnpm publish -r --access public --no-git-checks`
