894fe843063e2e142efea189dc36f9cc4ef08663
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-23 04:04:14 +0800

    fix: release script

--- scripts/prompt.mjs
+++ scripts/prompt.mjs
@@ -1,15 +1,20 @@
 // @ts-nocheck
 
 import { execSync } from 'child_process'
+import { join } from 'path'
+import fs from 'fs-extra'
 import { cd, $ } from 'zx'
 import { promptForPackage } from './packages.mjs'
 
 const pkg = await promptForPackage()
 
-const msg = `chore: release ${pkg.name} v`
-const tag = `${pkg.name}@v`
 execSync('pnpx bumpp', { stdio: 'inherit', cwd: pkg.path })
 cd(pkg.path)
+
+const { version } = await fs.readJSON(join(pkg.path, 'package.json'))
+
+const msg = `chore: release ${pkg.name} v${version}`
+const tag = `${pkg.name}@v${version}`
 await $`pnpx conventional-changelog -p angular -i CHANGELOG.md -s --commit-path . --lerna-package ${pkg.name}`
 await $`git add .`
 await $`git commit -m ${msg}`
